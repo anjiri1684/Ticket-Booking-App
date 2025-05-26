232c1f4b1dfed7a5cbc8f17652481d2cac78ae46
vincent anjiri <vincentanjiri12@gmail.com>
2025-05-26 11:14:34 +0300

    set up screen

---
+++ frontend/mobile/app/(authed)/(tabs)/(events)/new.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NewEvent() {
+     return (
+          <View>
+               <Text>New Event</Text>
+          </View>
+     )
+}
---
+++ frontend/mobile/app/(authed)/(tabs)/scan-ticket/_layout.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function ScanTicketScreen() {
+     return <>
+          <View>
+               <Text>Scan Ticket</Text>
+          </View>
+     </>
+}
